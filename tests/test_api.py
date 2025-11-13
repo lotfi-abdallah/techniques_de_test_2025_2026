3ed11987f52368596d92a3b8d0ef014ae776336d
Abdallah Lotfi <abdallahlotfi97@gmail.com>
2025-11-13 11:25:07 +0100

    test api call with mock of get_pointset to pointset manager

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,7 @@
 import sys
 from pathlib import Path
+from unittest.mock import patch
+from utils.to_binary_string import to_binary_string
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -20,9 +22,28 @@ from configure_tests import client,app
 #         - code
 #         - message
 
-def test_get_triangulation_nominal(client):
+def test_get_triangulation_one_triangle(client):
     """Test the /triangulation endpoint with a valid PointSet ID."""
-    response = client.get("/triangulation/123e4567-e89b-12d3-a456-426614174000")
+
+    fake_pointset = b'\x00\x00\x00\x03' + b'\x00\x00\x00\x00\x00\x00\x00\x00' + b'\x00\x00\x80?\x00\x00\x00@' + b'\x00\x00\x80@\x00\x00\x00@' + b'\x00\x00\x00\x01' + b'\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x02'
+    with patch('app.get_pointset', return_value=fake_pointset):
+        response = client.get("/triangulation/123e4567-e89b-12d3-a456-426614174000")
+    print(response.data)
+    assert response.status_code == 200
+    
+    assert response.content_type == 'application/octet-stream'
+    data = response.data
+    assert isinstance(data, bytes)
+
+def test_get_triangulation_no_triangles(client):
+    """Test the /triangulation endpoint with a PointSet that has no triangles."""
+
+    pointset = [(0.0, 0.0), (1.0, 0.0)]
+    fake_pointset = to_binary_string(pointset, [])
+
+    
+    with patch('app.get_pointset', return_value=fake_pointset):
+        response = client.get("/triangulation/123e4567-e89b-12d3-a456-426614174001")
     assert response.status_code == 200
     
     assert response.content_type == 'application/octet-stream'
@@ -37,7 +58,8 @@ def test_get_triangulation_invalid_id(client):
     #         application/json:
     #           schema:
     #             $ref: '#/components/schemas/Error'
-    response = client.get("/triangulation/invalid-id")
+    with patch('app.get_pointset', side_effect=FileNotFoundError):
+        response = client.get("/triangulation/invalid-id")
     assert response.status_code == 404
 
     assert response.content_type == 'application/json'
